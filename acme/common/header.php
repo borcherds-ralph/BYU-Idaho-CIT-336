bca21bb16c390f886aff6ce129599db734edd6a7
Ralph Borcherds <ralph@mrbconsulting.biz>
2017-11-15 21:54:23 -0800

    CSS Fixes

--- acme/common/header.php
+++ acme/common/header.php
@@ -3,7 +3,7 @@
             
                 <div class="logo"><a href="<?php echo $basepath; ?>"><img src="<?php echo $basepath; ?>/images/site/logo.gif" id="logo" alt="ACME Logo"></a></div>
                 <div class="right">
-                   
+                    <div class="account">
                         <?php if(isset($cookieFirstname)){
                             echo "<span id='welcome'>Welcome $cookieFirstname</span>";
                         } ?>
@@ -12,11 +12,11 @@
                                 echo "<span><div id='logout'><a href='$basepath/accounts/index.php?action=Logout'>Logout</a></div>";
                                 echo "<div id='update'><a href='$basepath/accounts/index.php?action=user-mgt'>Update Account</a></div></span>";
                             } else {
-                                echo "<span><a href='$basepath/accounts/index.php?action=login' class='link'>
-                                <img src='$basepath/images/site/account.gif' alt='Account Folder GIF' id='folder'>My Account</a></span>";
+                                echo "<a href='$basepath/accounts/index.php?action=login' class='link'>
+                                <img src='$basepath/images/site/account.gif' alt='Account Folder GIF' id='folder'>My Account</a>";
                             }
                         ?>
-                    
+                    </div>
                 </div>
                 
             </div>
